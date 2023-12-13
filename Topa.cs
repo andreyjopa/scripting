e916447d8fc87145459fcbdf6559ed456aee1868
andreyjopa <153101616+andreyjopa@users.noreply.github.com>
2023-12-13 18:33:26 +0500

    neznau

---
+++ Topa.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Topa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(topa);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
